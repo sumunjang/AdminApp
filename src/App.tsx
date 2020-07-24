603267ecf6989f98338d04f00ff9cb5fe0e8edea
DESKTOP-ET07CGU\Junsik Choi <2318ws@naver.com>
2020-07-25 06:53:44 +0900

    feat:login error handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Router from "../src/Components/Router";
 import { Link } from 'react-router-dom';
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    // font-family: 'Nanum Myeongjo', serif;
+    }
+`;
 
 const App: React.FunctionComponent = () => (
   <>
+    <GlobalStyle></GlobalStyle>
     <Router />
   </>
 );
-
 export default App;
--- src/Routes/AdminHome/AdminHomeContainer.tsx
+++ src/Routes/AdminHome/AdminHomeContainer.tsx
@@ -9,8 +9,14 @@ interface RootState {
     user: any
 }
 const request = async (id:string,pw:string)=>{
-    const res:{placeId:string,username:string,token:string} = await axios.post("http://34.105.29.115:3000/auth/signin/",{userid:id,password:pw});
-    return res;
+    try{
+        const res:{placeId:string,username:string,token:string} = await axios.post("http://34.105.29.115:3000/auth/signin/",{userid:id,password:pw});
+        return res;
+    }
+    catch(e){
+        alert("ID, PW를 확인해주세요.");
+        return null;
+    }
 }
 const AdminHomeContainer: React.FunctionComponent = () => {
     const user = useSelector((state:RootState)=>state.user);
